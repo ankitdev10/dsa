a6cd7b9e14154c1af28889b6e3eb6198fa023d09
ankitdev10 <leoankit35@gmail.com>
2023-05-28 19:32:48 +0545

    implemented bubble sort

---
+++ advancedJS.js
@@ -0,0 +1,32 @@
+// ! REST AND SPREAD OPERAOTRs
+//  ! local and global scope
+// ! CLOSURES
+// ! EVENT LOOP
+// ! DEBOUNCING
+
+//  ? REST AND SPREAD OPERATOR
+//  when you dont know the exact number of args that could be in our function, we can use rest operator
+
+// const myFunc = (...args) => {
+//   let obj = {};
+//   for (let el of args) {
+//     obj[el] = el;
+//     console.log(obj);
+//   }
+// };
+
+// myFunc("a", "b", "c");
+
+//  ! local and global scope
+
+// The main differences between var, let, and const in JavaScript are related to their scope, hoisting behavior, and reassignment capabilities:
+
+// Scope:
+// var variables are function-scoped. They are accessible throughout the entire function in which they are declared, and if declared outside of any function, they become globally scoped.
+// let and const variables are block-scoped. They are limited to the block (enclosed within curly braces) in which they are declared, such as within loops or if statements.
+// Hoisting:
+// var declarations are hoisted to the top of their function or global scope. This means you can access and assign a value to a var variable before it is declared in the code. However, the initial value will be undefined until it is assigned a value.
+// let and const declarations are also hoisted, but they are not initialized. This means you cannot access or assign a value to a let or const variable before it is declared in the code. It will result in a ReferenceError if you try to access it before the declaration.
+// Reassignment:
+// var and let variables can be reassigned with a new value after declaration.
+// const variables, as the name suggests, are constants and cannot be reassigned once they have been assigned a value. However, it's important to note that if a const variable holds a reference to an object, the properties of the object can still be modified.
--- practice.js
+++ practice.js
@@ -1,59 +1,3 @@
-// ! GIVEN TWO ARRAYS, CHECK IF ARR2 CONTAINS SQUARED ELEMENTS FROM ARRAY 1
-
-//? NAIVE SOLUTION
-
-// const same = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) return false;
-
-//   arr1 = arr1.sort((a, b) => a - b);
-//   arr2 = arr2.sort((a, b) => a - b);
-//   for (let i = 0; i < arr1.length; i++) {
-//     if ((arr2[i] == arr1[i]) * arr1[i]) {
-//       return true;
-//     }
-//     return false;
-//   }
-// };
-
-// console.log(same([1, 1, 2, 3], [1, 1, 9, 4]));
-
-// ! Frequency Counter - sameFrequency
-// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
-
-// Your solution MUST have the following complexities:
-
-// Time: O(N)
-
-// Sample Input:
-
-// sameFrequency(182,281) // true
-// sameFrequency(34,14) // false
-// sameFrequency(3589578, 5879385) // true
-// sameFrequency(22,222) // false
-
-// const sameFrequency = (num1, num2) => {
-//   const strnum1 = num1.toString();
-//   const strnum2 = num2.toString();
-//   let freqCount = {};
-
-//   if (strnum1.length !== strnum2.length) return false;
-
-//   for (let el of strnum1) {
-//     freqCount[el] = (freqCount[el] || 0) + 1;
-//   }
-
-//   for (let el of strnum2) {
-//     console.log(freqCount);
-//     if (!freqCount[el]) {
-//       return false;
-//     }
-//     freqCount[el]--;
-//   }
-//   return true;
-// };
-
-// console.log(sameFrequency(18, 81));
-
 // There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 // Example
@@ -115,7 +59,7 @@
 
 // console.log(romanToInt("VI"));
 
-// ! valid parentheses
+// !LEET CODE valid parentheses
 // var isValid = function (s) {
 //   let mapping = { ")": "(", "}": "{", "]": "[" };
 //   let stack = [];
@@ -137,3 +81,40 @@
 // };
 
 // console.log(isValid("({{{{}}}))"));
+
+// ! FIBONACCI WITHOUT RECURSION
+
+// function fib(n) {
+//   if (n == 1) return 0;
+//   if (n < 3) return 1;
+
+//   let first = 0;
+//   let second = 1;
+//   let third = 0;
+//   for (let i = 0; i < n - 1; i++) {
+//     third = first + second;
+//     first = second;
+//     second = third;
+//   }
+//   return third;
+// }
+
+// console.log(fib(35));
+
+// ! BUBBLE SORT
+
+const sort = (arr) => {
+  let temp;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(sort([100, 1, 121, 12, 2, 6, 7, 1]));
