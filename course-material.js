a9c70ed4900bf24f9c3ad907e213a19c0f7b6678
ankitdev10 <leoankit35@gmail.com>
2023-06-02 09:38:20 +0545

    solved palindrome

--- course-material.js
+++ course-material.js
@@ -118,3 +118,13 @@
 // };
 
 // console.log(reverse("cat"));
+
+// ! isPalindrome
+// Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.\
+
+// const isPal = (str) => {
+//   if (str.length == 0) return true;
+//   if (str[0] !== str[str.length - 1]) return false;
+
+//   return isPal(str.slice(1, str.length - 1));
+// };
