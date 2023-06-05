d102df841cea0e88a9524e0f542153dda3fbc86c
ankitdev10 <leoankit35@gmail.com>
2023-06-05 20:55:51 +0545

    started in java

---
+++ dsajava/singlyLinkedList/LL.java
@@ -0,0 +1,45 @@
+package dsajava.singlyLinkedList;
+
+public class LL {
+    Node head;
+    Node tail;
+    int length = 0;
+
+    class Node {
+        int data;
+        // ! because "next" is a varaible which stores next node so the type is given
+        Node next;;
+
+        // ? constructor for node
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    void push(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            // yedi list empty xaina bhani aile ko tail(last node of the list) is now second
+            // last tei bhayera aile ko tail ko next le newNode lai point garxa ani aaba
+            // naya tail bhaneko newNode hunxa
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void printItems() {
+        // print garna paila head bata suru ani hudai janxa, tei bhara euta variable
+        // chaiyo of data type Node itself which will traerse through the nodes
+        Node currNode = head;
+
+        while (currNode != null) {
+            System.out.println(currNode.data);
+            currNode = currNode.next;
+        }
+
+    }
+}
---
+++ dsajava/singlyLinkedList/Main.java
@@ -0,0 +1,18 @@
+package dsajava.singlyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LL linkedList = new LL();
+
+        linkedList.push(1);
+        linkedList.push(2);
+        linkedList.push(3);
+        linkedList.push(4);
+        linkedList.push(5);
+        linkedList.push(6);
+        linkedList.push(7);
+
+        linkedList.printItems();
+
+    }
+}
