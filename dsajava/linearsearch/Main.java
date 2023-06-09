6758140abd9919bd95cb42d9322bef8e7ad3d936
ankitdev10 <leoankit35@gmail.com>
2023-06-09 13:51:53 +0545

    completed linear search

--- dsajava/linearsearch/Main.java
+++ dsajava/linearsearch/Main.java
@@ -2,6 +2,16 @@ package dsajava.linearsearch;
 
 public class Main {
     public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        System.out.println(linearSearch(nums, 10));
+    }
 
+    // search in the array
+    static int linearSearch(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
+        return -1;
     }
 }
