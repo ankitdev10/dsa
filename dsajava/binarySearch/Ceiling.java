ee309ed3ddf25e706ad3d5a39a038e4d7e2700ca
ankitdev10 <leoankit35@gmail.com>
2023-06-09 20:02:00 +0545

    ceiling

---
+++ dsajava/binarySearch/Ceiling.java
@@ -0,0 +1,29 @@
+package dsajava.binarySearch;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 5, 6 };
+        System.out.println(getCeil(nums, 4));
+    }
+
+    static int getCeil(int[] nums, int t) {
+
+        int start = 0;
+        int end = nums.length - 1;
+        int middleIndex = 0;
+        while (start <= end) {
+            middleIndex = (start + end) / 2;
+            int middleNum = nums[middleIndex];
+
+            if (t > middleNum) {
+                start = middleIndex + 1;
+            } else if (t < middleNum) {
+                end = middleIndex - 1;
+            } else if (middleNum == t) {
+                return nums[middleIndex];
+            }
+        }
+
+        return nums[middleIndex];
+    }
+}
