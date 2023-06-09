3f58fb1fe83e72216b155d49e61f4e7826506e99
ankitdev10 <leoankit35@gmail.com>
2023-06-09 20:51:15 +0545

    completed leet code qn numn34

---
+++ dsajava/leetcodejava/Leetcode.java
@@ -0,0 +1,51 @@
+package dsajava.leetcodejava;
+
+public class Leetcode {
+    public static void main(String[] args) {
+
+    }
+
+    // ! leet code 744
+    // static char getChar(char[] letters, char target) {
+    // int start = 0;
+    // int end = letters.length - 1;
+    // int mid = 0;
+    // if (target >= letters[letters.length - 1])
+    // return letters[0];
+    // while (start <= end) {
+    // mid = (start + end) / 2;
+    // if (target < letters[mid]) {
+    // end = mid - 1;
+    // } else {
+    // start = mid + 1;
+    // }
+
+    // }
+    // return letters[start % letters.length];
+    // }
+
+    static int check(int[] nums, int target, boolean findStartIndex) {
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // possible answer
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
