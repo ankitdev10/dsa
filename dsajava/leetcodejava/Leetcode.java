1933116df31d43e642efe044097ccc8ef0761f99
ankitdev10 <leoankit35@gmail.com>
2023-06-14 14:51:28 +0545

    bs

--- dsajava/leetcodejava/Leetcode.java
+++ dsajava/leetcodejava/Leetcode.java
@@ -2,7 +2,7 @@ package dsajava.leetcodejava;
 
 public class Leetcode {
     public static void main(String[] args) {
-        int[] nums = { 4, 5, 6, 7, 3, 2, 1 };
+        int[] nums = { 3, 1 };
         Funcs func = new Funcs();
         System.out.println(func.peakIndexInMountainArray(nums));
     }
