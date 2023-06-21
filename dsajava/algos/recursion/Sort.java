fc09ab32dae955ea4654a7c61aa7d5640d1799f1
ankitdev10 <leoankit35@gmail.com>
2023-06-21 12:11:21 +0545

    merge sort

---
+++ dsajava/algos/recursion/Sort.java
@@ -0,0 +1,66 @@
+package dsajava.algos.recursion;
+
+import java.util.Arrays;
+
+// divide array into two parts
+
+// sort first half using recursion
+
+// sort second half
+
+// merge both sorted parts 
+
+public abstract class Sort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+        arr = mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] mergeSort(int[] arr) {
+        if (arr.length == 1)
+            return arr;
+
+        int mid = arr.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        System.out.println(Arrays.toString(left));
+        return merge(left, right);
+    }
+
+    static int[] merge(int[] first, int[] second) {
+
+        int[] mix = new int[first.length + second.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        // the above loop might have elements remaiing in either of the array so check
+        // that
+
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+
+}
