f7ebaeb9a37c4745b5c53e57fbcea2c81e8391c5
ankitdev10 <leoankit35@gmail.com>
2023-07-18 21:04:04 +0545

    array ques

--- dsajava/Datastructres/arrays/Funcs.java
+++ dsajava/Datastructres/arrays/Funcs.java
@@ -319,4 +319,30 @@ public class Funcs {
 
     }
 
+    // ! 152. Maximum Product Subarray
+
+    public int maxProduct(int[] nums) {
+        int largestProduct = nums[0];
+        int minCurrentProduct = nums[0];
+        int maxCurrentProduct = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int currentProduct = Math.max(maxCurrentProduct * nums[i], nums[i]);
+
+            if (minCurrentProduct * nums[i] > currentProduct)
+                currentProduct = minCurrentProduct * nums[i];
+
+            int temp = Math.min(maxCurrentProduct * nums[i], nums[i]);
+
+            if (minCurrentProduct * nums[i] > temp)
+                minCurrentProduct = temp;
+            else
+                minCurrentProduct = minCurrentProduct * nums[i];
+
+            maxCurrentProduct = currentProduct;
+            largestProduct = Math.max(maxCurrentProduct, largestProduct);
+        }
+
+        return largestProduct;
+    }
 }
