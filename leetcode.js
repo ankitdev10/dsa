04cf1799ea85ef7bdebcfb7b103a71d3c26e1f01
ankitdev10 <leoankit35@gmail.com>
2023-07-29 20:11:27 +0545

    strings

--- leetcode.js
+++ leetcode.js
@@ -229,14 +229,14 @@
 
 // ! 27. Remove Element
 
-// var removeElement = function (nums, val) {
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] === val) {
-//       nums.splice(i, 1);
-//       i--;
-//     }
-//   }
-//   return nums.length;
-// };
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
 
 //  ! 28. Find the Index of the First Occurrence in a String
